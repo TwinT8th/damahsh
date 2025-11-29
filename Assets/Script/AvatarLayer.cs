e7278b75fe85a6a821a8fb8bd13839472d0a70c3
TwinT8th <jihoamon@gmail.com>
2025-11-30 02:39:14 +0900

    아바타 파츠 분리 및 애니메이션 이벤트 추가 완

--- Assets/Script/AvatarLayer.cs
+++ Assets/Script/AvatarLayer.cs
@@ -1,39 +1,42 @@
 using UnityEngine;
+using System.Linq;
 
 public class AvatarLayer : MonoBehaviour
 {
     [Header("Sprite Renderers by Layer")]
     public SpriteRenderer bodyRenderer;
-    public SpriteRenderer hairRenderer;
-    public SpriteRenderer outfitRenderer;
-    public SpriteRenderer accessoryRenderer;
+    public SpriteRenderer headRenderer;
+    public SpriteRenderer hairFrontRenderer;
+    public SpriteRenderer hairBackRenderer;
+    public SpriteRenderer upperRenderer;
+    public SpriteRenderer underRenderer;
 
-    [Header("Animation State Name")]
-    public string currentAnimState = "Idle"; // Idle / Walk µî
+    [Header("Sprite Arrays (Assign in Inspector)")]
+    public Sprite[] bodySprites;
+    public Sprite[] headSprites;
+    public Sprite[] hairFrontSprites;
+    public Sprite[] hairBackSprites;
+    public Sprite[] upperSprites;
+    public Sprite[] underSprites;
 
-    public void ChangeOutfit(string outfitName)
+    public void SetFrame(int index)
     {
-        Sprite[] sprites = Resources.LoadAll<Sprite>($"Character/Outfit/{currentAnimState}/{outfitName}");
-        outfitRenderer.sprite = sprites.Length > 0 ? sprites[0] : null;
+        if (bodySprites.Length > index)
+            bodyRenderer.sprite = bodySprites[index];
+
+        if (headSprites.Length > index)
+            headRenderer.sprite = headSprites[index];
+
+        if (hairFrontSprites.Length > index)
+            hairFrontRenderer.sprite = hairFrontSprites[index];
+
+        if (hairBackSprites.Length > index)
+            hairBackRenderer.sprite = hairBackSprites[index];
+
+        if (upperSprites.Length > index)
+            upperRenderer.sprite = upperSprites[index];
+
+        if (underSprites.Length > index)
+            underRenderer.sprite = underSprites[index];
     }
-
-    public void ChangeHair(string hairName)
-    {
-        Sprite[] sprites = Resources.LoadAll<Sprite>($"Character/Hair/{currentAnimState}/{hairName}");
-        hairRenderer.sprite = sprites.Length > 0 ? sprites[0] : null;
-    }
-
-    public void ChangeAccessory(string accName)
-    {
-        Sprite[] sprites = Resources.LoadAll<Sprite>($"Character/Accessories/{currentAnimState}/{accName}");
-        accessoryRenderer.sprite = sprites.Length > 0 ? sprites[0] : null;
-    }
-
-    public void SetAnimState(string animState)
-    {
-        currentAnimState = animState;
-    }
-
-
-
 }
